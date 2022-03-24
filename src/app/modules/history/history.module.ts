81dccb0ec01918121e8a905c6f58b91372bf5335
Saravana Raja <saravanaraja25@gmail.com>
2022-03-24 16:22:19 +0530

    history design complete

--- src/app/modules/history/history.module.ts
+++ src/app/modules/history/history.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HistoryRoutingModule } from './history-routing.module';
 import { HistoryComponent } from './history.component';
 import { HistoryItemComponent } from './history-item/history-item.component';
+import { HistoryFilterComponent } from './history-filter/history-filter.component';
 
 
 @NgModule({
   declarations: [
     HistoryComponent,
     HistoryItemComponent,
+    HistoryFilterComponent,
   ],
   imports: [
     CommonModule,
