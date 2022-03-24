df075dbcc62cbf7d4e6077a8c779567ed3e4d7d1
Yashwanth K <yashwanthkkn@gmail.com>
2022-03-24 17:02:23 +0530

    added thumbnail cards

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CardComponent } from './components/card/card.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
