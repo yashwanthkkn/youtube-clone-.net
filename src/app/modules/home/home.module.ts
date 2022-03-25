b49df5b1ed8d1026773e04c82f531cd0cfde762d
Yashwanth K <yashwanthkkn@gmail.com>
2022-03-25 13:30:22 +0530

    added popup to thumbnail card

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { CardComponent } from './components/card/card.component';
+import { HomePopupComponent } from './components/home-popup/home-popup.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    CardComponent
+    CardComponent,
+    HomePopupComponent
   ],
   imports: [
     CommonModule,
