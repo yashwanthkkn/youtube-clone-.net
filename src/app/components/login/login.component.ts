fe42f5d66245e716b1b13d0f61fe0c1cb81ce16b
Yashwanth K <yashwanthkkn@gmail.com>
2022-03-28 14:02:57 +0530

    added oauth service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OauthService } from 'src/app/services/oauth.service';
+import { YoutubeService } from 'src/app/services/youtube.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:OauthService,private api: YoutubeService) { }
 
   ngOnInit(): void {
+  }
+
+  signIn(){
+    this.authService.signIn()
+      .then((res)=>{
+        console.log(res);      
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+  }
+
+  getVids(){
+    this.api.getRandomVideos();
   }
 
 }
