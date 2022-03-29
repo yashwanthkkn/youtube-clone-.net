ea81acf309a575c45e468ad7d36ed9d2414be280
Saravana Raja <saravanaraja25@gmail.com>
2022-03-29 22:45:31 +0530

    router link changes home

--- src/app/modules/home/components/card/card.component.ts
+++ src/app/modules/home/components/card/card.component.ts
@@ -8,9 +8,17 @@ export class CardComponent implements OnInit {
 
   isLoading: boolean = true;
   @Input() cardData : any;
+
+  id: string = '';
   constructor() {}
 
   ngOnInit(): void {
+    if(typeof this.cardData.id == 'string'){
+      this.id = this.cardData.id;
+    }
+    else if(typeof this.cardData.id == 'object'){
+      this.id = this.cardData.id.videoId;
+    }
     setTimeout(() => {
       this.isLoading = false;
     }, 2000);
