88a08d8400bafb2fb699f58a35202ee6a37a06ad
Yashwanth K <yashwanthkkn@gmail.com>
2022-03-31 12:05:57 +0530

    subs list added

---
+++ src/app/services/subscription.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { OauthService } from './oauth.service';
+
+declare var gapi: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubscriptionService {
+
+  subscriptions:any = []
+  constructor(private authService : OauthService) { 
+    (async () =>{
+      // loading client if not exists
+      if( ! gapi.client ){
+        await this.authService.loadClient();
+      }
+      // if already authenticated fill the subscriptions
+      if(authService.isAuthenticated){
+        this.subscriptions = await this.getSubscriptions()
+      }
+      // clear subscriptions on logout and on login, fill them if not already been filled
+      authService.GoogleAuth?.isSignedIn.listen(async (signIn:boolean)=>{
+        if(!signIn){
+          this.subscriptions = []
+        }else if(this.subscriptions.length == 0){
+          this.subscriptions = await this.getSubscriptions()
+        }
+      })
+    })();
+  }
+
+  async getSubscriptions(){
+    
+    if( ! gapi.client ){
+      await this.authService.loadClient();
+    }
+    let subscriptions: any[] = [];
+    let params = {
+      part : 'snippet',
+      mine : 'true',
+    }
+
+    if(!this.authService.isAuthenticated){
+      return subscriptions
+    }
+    
+    let response = await gapi.client.request({
+      'method': 'GET',
+      'path': '/youtube/v3/subscriptions',
+      'params': params,
+      
+    });
+    subscriptions = response.result.items
+    return subscriptions
+  }
+}
